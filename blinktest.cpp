7e59a06c59a122f3b650ffa8cbd18d706d754d91
Andy Roggenbuck <andy_roggenbuck@hotmail.com>
2020-12-02 00:37:04 -0500

    first commit

---
+++ blinktest.cpp
@@ -0,0 +1,23 @@
+#include "avr/io.h"
+
+void delay(unsigned long time) 
+{
+    while (time > 0) 
+    {
+        time--;
+        __asm ("nop");
+    }
+}
+
+int main(void) 
+{
+    DDRB |= (1 << PORTB5);      // port B pin 5 set to output
+
+    while (1) 
+    {
+        PORTB ^= (1 << PORTB5);  // toggle port B pin 5
+        delay(1000000);
+    }
+
+    return 0;
+}
